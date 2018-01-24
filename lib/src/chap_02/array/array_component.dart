22a4b022286bce33fcfa8cfb4f41d643d4683ee4
artem-galas <artem.galas@gmail.com>
2018-01-24 21:20:44 +0200

    Adding 'NEW' button functionality MinnorImprove style

--- lib/src/chap_02/array/array_component.dart
+++ lib/src/chap_02/array/array_component.dart
@@ -21,11 +21,16 @@ class ArrayComponent implements OnInit {
   Random random = new Random();
   String message;
   int listIndex;
+  int _codePart;
+  String userNumber;
+  int _totalArraySize;
+
   ArrayComponent() {
     message = 'Press any button';
+    _codePart = 1;
     items = new List<Item>();
     listIndex = 0;
-    for(int i = 0; i < 10; i ++) {
+    for(int i = 0; i < 20; i ++) {
       items.add(new Item(random.nextInt(1000)));
     }
   }
@@ -38,5 +43,34 @@ class ArrayComponent implements OnInit {
       listIndex = 0;
     else 
       listIndex ++;
+  }
+
+  void newArray() {
+    switch(_codePart) {
+      case 1:
+        message = 'Enter size of array to create';
+        _totalArraySize = int.parse(userNumber);
+        _codePart = 2;
+        return;
+      case 2: 
+        if(userNumber.length > 0) {
+          message = 'Will create empty arrayw with $userNumber cells';
+          _codePart = 3;
+        } else {
+          _codePart = 1;
+        }
+        return;
+      case 3:
+        for(int i = 0; i < _totalArraySize; i++) {
+          items = new List<Item>(_totalArraySize);
+          message = 'New array created; total items 0';
+        }
+        _codePart = 6;
+        return;        
+      case 6: 
+        message = 'Press any button';
+        _codePart = 1;
+        return;
+    }
   }
 }
