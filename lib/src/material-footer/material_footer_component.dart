21c16eb1db65cfdbcb352a414be42ed6c129cd2d
artem-galas <artem.galas@gmail.com>
2018-01-30 19:58:37 +0200

    Add material-footer component

---
+++ lib/src/material-footer/material_footer_component.dart
@@ -0,0 +1,8 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'material-footer',
+  styleUrls: const ['material_footer_component.css'],
+  templateUrl: 'material_footer_component.html')
+class FooterComponent {}
+
