0fb17c1746d9d5e67581df1aee6c1f560f42c1a6
artem-galas <artem.galas@gmail.com>
2018-03-10 12:36:47 +0200

    Change Home page styles

--- lib/src/home/home_component.dart
+++ lib/src/home/home_component.dart
@@ -1,9 +1,13 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_button/material_button.dart';
 
 @Component(
   selector: 'home',
   styleUrls: const ['home_component.css'],
   templateUrl: 'home_component.html',
+  directives: const[
+    MaterialButtonComponent,
+  ]
 )
 class HomeComponent {
   
